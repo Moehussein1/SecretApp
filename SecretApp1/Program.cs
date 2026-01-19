ce2215483e4da4b909fd7a462d4281d6a40af196
Mohammad Hussein <Zestyan@gmail.com>
2026-01-19 13:28:40 +0100

    Add input validation and menu choices

--- SecretApp1/Program.cs
+++ SecretApp1/Program.cs
@@ -11,12 +11,36 @@ namespace SecretApp1
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Hej");
+            Thread.Sleep(3000);
             bool runProgram = true;
           while (runProgram) 
             {
-                
+                Console.WriteLine("Välj 0 eller 1");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    if (choice == 1)
+                    {
+                        Console.WriteLine("Du valde 1");
+                    }
+                    else if (choice == 0)
+                    {
+                        runProgram = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Välj i menyn");
+                    {
+                        Console.WriteLine("Hej då");
+                        Thread.Sleep(3000);
+                    }
+                }
             }
         }
+        
+
         static void AddUser()
         {
             Console.WriteLine("Hello from AddUser");
