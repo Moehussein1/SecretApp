e756c1854d0a5d219fe907980173076a3cb8e6a9
Mohammad Hussein <Zestyan@gmail.com>
2026-01-19 14:42:51 +0100

    Add text-based menu with while loop and input validation

--- SecretApp1/Program.cs
+++ SecretApp1/Program.cs
@@ -1,70 +1,85 @@
-﻿using System.ComponentModel.Design;
-using System.Net.Http.Json;
-
-namespace SecretApp1
+﻿namespace SecretAppla
 {
     internal class Program
     {
-
         static string[] userNames = { "Pelle", "Stina", "Ali" };
-        static string[] userPasswords = { "1234", "abcd", "qwerty" };
+        static string[] userPasswords = { "1234", "12345", "12346" };
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hej");
-            Thread.Sleep(3000);
-            bool runProgram = true;
-          while (runProgram) 
+
+            bool runRrogram = true;
+            while (runRrogram)
             {
-                Console.WriteLine("Välj 0 eller 1");
+                Console.WriteLine("" +
+                    "1. Logga in\r\n" +
+                    "2. Lägg till användare\r\n" +
+                    "3. Ändra lösenord\r\n" +
+                    "4. Vissa användar Lista\r\n" +
+                    "0. Avsluta\r\n");
 
                 if (int.TryParse(Console.ReadLine(), out int choice))
                 {
                     if (choice == 1)
                     {
-                        Console.WriteLine("Du valde 1");
+                        LoggIn();
+                    }
+                    else if (choice == 2)
+                    {
+                        AddUser();
+                    }
+                    else if (choice == 3)
+                    {
+                        ChangePassword();
+                    }
+                    else if (choice == 4)
+                    {
+                        ShowUsers();
                     }
                     else if (choice == 0)
                     {
-                        runProgram = false;
+                        runRrogram = false;
                     }
                 }
                 else
                 {
                     Console.WriteLine("Välj i menyn");
-                    {
-                        Console.WriteLine("Hej då");
-                        Thread.Sleep(3000);
-                    }
                 }
             }
+
+            Console.WriteLine("Hej då");
+            Thread.Sleep(3000);
         }
-        
 
         static void AddUser()
         {
-            Console.WriteLine("Hello from AddUser");
-        }
-
-        static void ChangePassword()
-        {
-            Console.WriteLine("Hello from ChangePassword");
+            Console.WriteLine("Hello from AddUser()");
         }
 
         static void ShowUsers()
         {
-           int i = 0;
-            while(i < userNames.Length)
+            int i = 0;
+            while (i < userNames.Length)
             {
                 Console.WriteLine(userNames[i].ToUpper());
+                i++;
             }
-            
+        }
+
+        static void LoggIn()
+        {
+            Console.WriteLine("Helo from LoggIn");
+        }
+
+        static void ChangePassword()
+        {
+            Console.WriteLine("Hello from ChangePassword()");
         }
 
         static void EndApplication()
         {
-            Console.WriteLine("Hello from EndApplication");
+            Console.WriteLine("Hello from EndApplication()");
         }
-
     }
 }
